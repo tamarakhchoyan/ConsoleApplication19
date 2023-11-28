c59decefd5d5e0fdd0fa99591652148e4dc32be1
User <User@DESKTOP-9TGTEUB>
2023-11-28 11:05:49 +0400

    Add project files.

---
+++ ConsoleApplication19/ConsoleApplication19.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+	int a = 3;
+	int b = 4;
+	double c = 5;
+	int p = a + b + c;
+	int s = a * b / 2;
+	double sin = a / c;
+	double cos = b / c;
+	int h = a * b / c;
+	std::cout << p << std::endl;
+	std::cout << s << std::endl;
+	std::cout << sin << std::endl;
+	std::cout << cos << std::endl;
+	std::cout << h << std::endl;
+	return 0;
+}
